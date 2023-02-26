c348bc1fbab6a335211939f601b8c4aadb66186f
zhiyi-zhang-duke <zhiyi.zhang.duke@gmail.com>
2023-02-26 15:40:37 -0500

    Setting up repo where next/image doesn't work

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       Hello World. <Link href="/about">About</Link>
+      <img src="/img1.jpg" alt='' />
     </div>
   )
 }
